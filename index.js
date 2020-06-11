798b0af6740ce6518a97c723bc14207518d59b66
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-06-11 21:43:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,5 +17,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  
+  if(string.toLowerCase() === string) {
+    return 'I can\'t hear you!'
+  } else if (string.toUpperCase === string) {
+    
+  }
 }
