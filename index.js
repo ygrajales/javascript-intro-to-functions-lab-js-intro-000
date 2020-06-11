33a268d1f5b72e62ba0ea124968bd812659b26b1
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-06-11 21:35:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+  return
+}
+
+function logWhisper(string) {
+  
+}
